9d34358460422c1a0ed3a9d72c159b85608478a2
Hubert Nachyla <hubertnachyla66@gmail.com>
2022-01-20 16:05:37 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let button = document.querySelector(".button")
+let background = document.querySelector(".background")
+let themeName = document.querySelector(".themeName")
+
+button.addEventListener("click", () => {
+    background.classList.toggle("dark");
+
+    if (background.classList.contains("dark")) {
+        themeName.innerText = ("jasne");
+} else {
+    themeName.innerText = ("ciemne");
+
+}
+
+
+});
